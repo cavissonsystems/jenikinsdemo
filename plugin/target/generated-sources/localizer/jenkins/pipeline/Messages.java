1c4fef19b150201f3983ddc9eeb3a4b95aa76dc2
Vibhor Agarwal <agarwalvibhor99@gmail.com>
2019-06-25 12:54:22 +0530

    inital commit

---
+++ plugin/target/generated-sources/localizer/jenkins/pipeline/Messages.java
@@ -0,0 +1,222 @@
+// CHECKSTYLE:OFF
+
+package jenkins.pipeline;
+
+import org.jvnet.localizer.Localizable;
+import org.jvnet.localizer.ResourceBundleHolder;
+import org.kohsuke.accmod.Restricted;
+
+
+/**
+ * Generated localization support class.
+ * 
+ */
+@SuppressWarnings({
+    "",
+    "PMD",
+    "all"
+})
+@Restricted(org.kohsuke.accmod.restrictions.NoExternalUse.class)
+public class Messages {
+
+    /**
+     * The resource bundle reference
+     * 
+     */
+    private final static ResourceBundleHolder holder = ResourceBundleHolder.get(Messages.class);
+
+    /**
+     * Key {@code ResultsGraphConfiguration.DisplayName}: {@code Configure
+     * NetStorm Performance Graph}.
+     * 
+     * @return
+     *     {@code Configure NetStorm Performance Graph}
+     */
+    public static String ResultsGraphConfiguration_DisplayName() {
+        return holder.format("ResultsGraphConfiguration.DisplayName");
+    }
+
+    /**
+     * Key {@code ResultsGraphConfiguration.DisplayName}: {@code Configure
+     * NetStorm Performance Graph}.
+     * 
+     * @return
+     *     {@code Configure NetStorm Performance Graph}
+     */
+    public static Localizable _ResultsGraphConfiguration_DisplayName() {
+        return new Localizable(holder, "ResultsGraphConfiguration.DisplayName");
+    }
+
+    /**
+     * Key {@code NetDiagnosticsResultsPublisher.DisplayName}: {@code
+     * NetDiagnostics Performance Publisher}.
+     * 
+     * @return
+     *     {@code NetDiagnostics Performance Publisher}
+     */
+    public static String NetDiagnosticsResultsPublisher_DisplayName() {
+        return holder.format("NetDiagnosticsResultsPublisher.DisplayName");
+    }
+
+    /**
+     * Key {@code NetDiagnosticsResultsPublisher.DisplayName}: {@code
+     * NetDiagnostics Performance Publisher}.
+     * 
+     * @return
+     *     {@code NetDiagnostics Performance Publisher}
+     */
+    public static Localizable _NetDiagnosticsResultsPublisher_DisplayName() {
+        return new Localizable(holder, "NetDiagnosticsResultsPublisher.DisplayName");
+    }
+
+    /**
+     * Key {@code NetStormBuilder.Task}: {@code Execute NetStorm Test}.
+     * 
+     * @return
+     *     {@code Execute NetStorm Test}
+     */
+    public static String NetStormBuilder_Task() {
+        return holder.format("NetStormBuilder.Task");
+    }
+
+    /**
+     * Key {@code NetStormBuilder.Task}: {@code Execute NetStorm Test}.
+     * 
+     * @return
+     *     {@code Execute NetStorm Test}
+     */
+    public static Localizable _NetStormBuilder_Task() {
+        return new Localizable(holder, "NetStormBuilder.Task");
+    }
+
+    /**
+     * Key {@code NetStormProjectAction.DisplayName}: {@code NetStorm
+     * Performance Trend}.
+     * 
+     * @return
+     *     {@code NetStorm Performance Trend}
+     */
+    public static String NetStormProjectAction_DisplayName() {
+        return holder.format("NetStormProjectAction.DisplayName");
+    }
+
+    /**
+     * Key {@code NetStormProjectAction.DisplayName}: {@code NetStorm
+     * Performance Trend}.
+     * 
+     * @return
+     *     {@code NetStorm Performance Trend}
+     */
+    public static Localizable _NetStormProjectAction_DisplayName() {
+        return new Localizable(holder, "NetStormProjectAction.DisplayName");
+    }
+
+    /**
+     * Key {@code NetStormBuildAction.DisplayName}: {@code NetStorm
+     * Performance Report}.
+     * 
+     * @return
+     *     {@code NetStorm Performance Report}
+     */
+    public static String NetStormBuildAction_DisplayName() {
+        return holder.format("NetStormBuildAction.DisplayName");
+    }
+
+    /**
+     * Key {@code NetStormBuildAction.DisplayName}: {@code NetStorm
+     * Performance Report}.
+     * 
+     * @return
+     *     {@code NetStorm Performance Report}
+     */
+    public static Localizable _NetStormBuildAction_DisplayName() {
+        return new Localizable(holder, "NetStormBuildAction.DisplayName");
+    }
+
+    /**
+     * Key {@code NetStormResultsPublisher.DisplayName}: {@code NetStorm
+     * Performance Publisher}.
+     * 
+     * @return
+     *     {@code NetStorm Performance Publisher}
+     */
+    public static String NetStormResultsPublisher_DisplayName() {
+        return holder.format("NetStormResultsPublisher.DisplayName");
+    }
+
+    /**
+     * Key {@code NetStormResultsPublisher.DisplayName}: {@code NetStorm
+     * Performance Publisher}.
+     * 
+     * @return
+     *     {@code NetStorm Performance Publisher}
+     */
+    public static Localizable _NetStormResultsPublisher_DisplayName() {
+        return new Localizable(holder, "NetStormResultsPublisher.DisplayName");
+    }
+
+    /**
+     * Key {@code NetDiagnosticsBuildAction.DisplayName}: {@code
+     * NetDiagnostics Performance Report}.
+     * 
+     * @return
+     *     {@code NetDiagnostics Performance Report}
+     */
+    public static String NetDiagnosticsBuildAction_DisplayName() {
+        return holder.format("NetDiagnosticsBuildAction.DisplayName");
+    }
+
+    /**
+     * Key {@code NetDiagnosticsBuildAction.DisplayName}: {@code
+     * NetDiagnostics Performance Report}.
+     * 
+     * @return
+     *     {@code NetDiagnostics Performance Report}
+     */
+    public static Localizable _NetDiagnosticsBuildAction_DisplayName() {
+        return new Localizable(holder, "NetDiagnosticsBuildAction.DisplayName");
+    }
+
+    /**
+     * Key {@code NetStormReport.DisplayName}: {@code NetStorm Report}.
+     * 
+     * @return
+     *     {@code NetStorm Report}
+     */
+    public static String NetStormReport_DisplayName() {
+        return holder.format("NetStormReport.DisplayName");
+    }
+
+    /**
+     * Key {@code NetStormReport.DisplayName}: {@code NetStorm Report}.
+     * 
+     * @return
+     *     {@code NetStorm Report}
+     */
+    public static Localizable _NetStormReport_DisplayName() {
+        return new Localizable(holder, "NetStormReport.DisplayName");
+    }
+
+    /**
+     * Key {@code NetDiagnosticsBuilder.Task}: {@code Execute NetDiagnostics
+     * Test}.
+     * 
+     * @return
+     *     {@code Execute NetDiagnostics Test}
+     */
+    public static String NetDiagnosticsBuilder_Task() {
+        return holder.format("NetDiagnosticsBuilder.Task");
+    }
+
+    /**
+     * Key {@code NetDiagnosticsBuilder.Task}: {@code Execute NetDiagnostics
+     * Test}.
+     * 
+     * @return
+     *     {@code Execute NetDiagnostics Test}
+     */
+    public static Localizable _NetDiagnosticsBuilder_Task() {
+        return new Localizable(holder, "NetDiagnosticsBuilder.Task");
+    }
+
+}
